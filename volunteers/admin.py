5c511ded27edbd448adb8e9abddb433aefafb235
Danilo Cabello <danilo.cabello@gmail.com>
2011-06-04 16:26:14 -0300

    Add prepopulated fields

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -5,10 +5,12 @@ from volunteers.models import Profile, Call, Skill, Resource
 
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
 
 
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Profile)
 admin.site.register(Call)
--- volunteers/models.py
+++ volunteers/models.py
@@ -3,11 +3,14 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    mobile = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=255, blank=True)
     where = models.CharField(max_length=255)
-    radius = models.CharField(max_length=255)
-    skills = models.ManyToManyField('Skill')
-    resources = models.ManyToManyField('Resource')
+    radius = models.CharField(max_length=255, default='1 KM')
+    skills = models.ManyToManyField('Skill', blank=True)
+    resources = models.ManyToManyField('Resource', blank=True)
+
+    def __unicode__(self):
+        return self.name
 
 class Call(models.Model):
     title = models.CharField(max_length=255)
@@ -15,8 +18,11 @@ class Call(models.Model):
     when = models.DateTimeField()
     classification = models.CharField(max_length=255)
     description = models.TextField()
-    skills_needed = models.ManyToManyField('Skill')
-    resources_needed = models.ManyToManyField('Resource')
+    skills_needed = models.ManyToManyField('Skill', blank=True)
+    resources_needed = models.ManyToManyField('Resource', blank=True)
+
+    def __unicode__(self):
+        return self.title
 
 class Skill(models.Model):
     name = models.CharField(max_length=255)
