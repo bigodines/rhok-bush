e2d107f8fd0ff87f775e0c010af46194a66373c7
Danilo Cabello <danilo.cabello@gmail.com>
2011-06-04 15:54:52 -0300

    Add first models Resource, Skill, Call, Volunteer

--- volunteers/models.py
+++ volunteers/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
 class Profile(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=255)
+    where = models.CharField(max_length=255)
+    radius = models.CharField(max_length=255)
+    skills = models.ManyToManyField('Skill')
     resources = models.ManyToManyField('Resource')
 
-class Resource(models.Model):
-    name = models.CharField(max_length=30)
+class Call(models.Model):
+    title = models.CharField(max_length=255)
+    where = models.CharField(max_length=255)
+    when = models.DateTimeField()
+    classification = models.CharField(max_length=255)
+    description = models.TextField()
+    skills_needed = models.ManyToManyField('Skill')
+    resources_needed = models.ManyToManyField('Resource')
 
-class Event(models.Model):
-    name = models.CharField(max_length=30)
-    date_time = models.DateTimeField()
-    resources_required = models.ManyToManyField('Resource')
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+class Resource(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
