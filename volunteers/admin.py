58388fc83bd6a3f70b393a3dce41510045105a9a
Danilo Cabello <danilo.cabello@gmail.com>
2011-06-04 16:44:49 -0300

    Add prepopulated slug fields for Profile and Call

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from volunteers.models import Profile, Call, Skill, Resource
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class CallAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
@@ -12,7 +20,8 @@ class ResourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Profile)
-admin.site.register(Call)
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Call, CallAdmin)
 admin.site.register(Skill, SkillAdmin)
 admin.site.register(Resource, ResourceAdmin)
