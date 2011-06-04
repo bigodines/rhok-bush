5ca4c68a61be6609f8a72dee41f47c83b4148140
Rodrigo Setti <rsetti@yahoo-inc.com>
2011-06-04 15:32:12 -0300

    bugfi manytomany

--- volunteers/models.py
+++ volunteers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length=30)
-    resources = models.OneToManyField('Resource')
+    resources = models.ManyToManyField('Resource')
 
 class Resource(models.Model):
     name = models.CharField(max_length=30)
@@ -10,4 +10,4 @@ class Resource(models.Model):
 class Event(models.Model):
     name = models.CharField(max_length=30)
     date_time = models.DateTimeField()
-    resources_required = models.OneToManyField('Resource')
+    resources_required = models.ManyToManyField('Resource')
