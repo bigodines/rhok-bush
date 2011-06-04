95f1bb151a37ca281dc1873bb456af0224c6b3a1
Danilo Cabello <danilo.cabello@gmail.com>
2011-06-04 16:08:42 -0300

    Add admin for Profile, Call

--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from volunteers.models import Skill, Resource
+from volunteers.models import Profile, Call, Skill, Resource
 
 
 class SkillAdmin(admin.ModelAdmin):
@@ -10,6 +10,7 @@ class SkillAdmin(admin.ModelAdmin):
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
 
-
+admin.site.register(Profile)
+admin.site.register(Call)
 admin.site.register(Skill, SkillAdmin)
 admin.site.register(Resource, ResourceAdmin)
